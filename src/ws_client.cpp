2c35d89a829b4bbdb997f04eaa07189e10af6ad6
Creeper-xie <Creeperxie@outlook.com>
2025-02-14 06:19:25 +0000

    将command和msg合并到ws_Client类

--- src/ws_client.cpp
+++ src/ws_client.cpp
@@ -42,7 +42,37 @@ int BotWebSocketClient::connect(const char* url,std::string token) {
         headers["Authorization"] = "Bearer " + token;
         return open(url, headers);
     };
+void BotWebSocketClient::handle(const string& msg,BotWebSocketClient* ws){
+    json jsonMsg = json::parse(std::move(msg));
+    if(jsonMsg.contains("post_type") && jsonMsg["post_type"].get<string>()=="message"){
+        string msgData = jsonMsg["raw_message"].get<string>();
+    if(msgData.empty() || !(msgData.at(0) == '>')) return;
+        string::size_type n = msgData.find(' ');
+        string result = command(msgData.substr(1,n-1),msgData.substr(n+1),ws->plugins,ws->commands);
+        if (result.empty()) return;
+        json sendMsg;
+        sendMsg["action"] = "send_msg";
+        if(jsonMsg["message_type"].get<string>() == "private"){
+            sendMsg["params"]["detail_type"] = "private";
+            sendMsg["params"]["user_id"] = jsonMsg["user_id"].get<uint64_t>();
+            } else if(jsonMsg["message_type"].get<string>() == "group"){
+            sendMsg["params"]["detail_type"] = "group";
+            sendMsg["params"]["group_id"] = jsonMsg["group_id"].get<uint64_t>();
+            }
+        sendMsg["params"]["message"][0]["type"] = "text";
+        sendMsg["params"]["message"][0]["data"]["text"] = result;
+        string str=sendMsg.dump();
+        printf("%s\n",str.data());
+        ws-> send(str);
+        }
+    };
 
 
+string BotWebSocketClient::command(const string& command,const string& arg,map<string,apiPtr>* plugins,map<string,string>* commands){
+   if(commands->contains(command)){
+        return ((*plugins)[(*commands)[command]] -> execute(command));
+    }
+    return string("");
+};
 #define WS_CLIENT_CPP
 #endif
--- src/ws_client.hpp
+++ src/ws_client.hpp
@@ -15,6 +15,8 @@ public:
     ~BotWebSocketClient();
 
     int connect(const char* url,std::string token);
+    void handle(const std::string& msg,BotWebSocketClient* ws);
+string command(const string& command,const string& arg,map<string,apiPtr>* plugins,map<string,string>* commands);
     std::map<std::string,std::string>* commands;
     std::map<string,apiPtr>* plugins;
 };
