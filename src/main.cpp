58551138b09b85e2c71770b8d3bee2a6231e2603
Creeper-xie <Creeperxie@outlook.com>
2025-04-03 09:26:52 +0000

    刪除：     ../config.toml 	刪除：     ../include/meson.build 	修改：     ../meson.build 	新檔案：   ../snwbot_config.toml 	重新命名： bot.hpp -> include/bot.hpp 	重新命名： ../include/plugin_api.hpp -> include/plugin_api.hpp 	修改：     main.cpp 	修改：     meson.build config file: 更改了配置文件

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <toml++/toml.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/dll/runtime_symbol_info.hpp>
@@ -7,29 +8,31 @@
 namespace fs = boost::filesystem;
 namespace sys = boost::system;
 
-fs::path plugin_dir() {
-    const fs::path executable_dir = boost::dll::program_location().parent_path();
-    const fs::path plugin_dir = executable_dir / "plugins";
-    // 刻意忽略错误处理，让程序在遇到错误时停止运行
-    return fs::canonical(plugin_dir);
-}
+using std::string;
+
+string config_path;
 
 int main(int argc, char** argv) {
   try {
-    const toml::table config = toml::parse_file("config.toml");
-    const std::string url = *config["bot"]["ws_url"].value<std::string>();
-    const std::string token = *config["bot"]["token"].value<std::string>();
+    if (argv[1] == "-c"){
+        config_path = argv[2];
+    } else {
+        config_path = "snwbot_config.toml";
+    }
+    const toml::table config = toml::parse_file(config_path);
+    const string url = *config["ws_url"].value<std::string>();
+    const string token = *config["token"].value<std::string>();
     Bot bot(NULL);
     bot.connect(url.data(),token);
 
-    const fs::path plugins_dir = plugin_dir();
+    const fs::path plugins_dir = *config["plugins_dir"].value<std::string>();
     for (auto &entry : fs::directory_iterator(plugins_dir)) {
         if(fs::is_regular_file(entry)){
             bot.load_plugin(entry);
         }
     }
 
-    std::string str;
+    string str;
     while (std::getline(std::cin, str)) {
         if (str == "close") {
             bot.close();
